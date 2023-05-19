dde5b2a65703f12af4bc776fe8a8b4e611bd1eae
Aidan Roche <aidanroche937@gmail.com>
2023-05-19 16:21:55 -0400

    Implemented QuestionAndAnswer. Fixes #4

---
+++ src/main/java/cs3500/pa02/fileformatters/QuestionAndAnswer.java
@@ -0,0 +1,87 @@
+package cs3500.pa02.fileformatters;
+
+/**
+ * Class for formatting a .sr file by extracting questions and answers
+ */
+public class QuestionAndAnswer extends FileFormat {
+
+  /**
+   * Initializes a QuestionAndAnswer
+   *
+   * @param content the content to be formatted
+   */
+  public QuestionAndAnswer(String content) {
+    super(content);
+  }
+
+  /**
+   * Returns the questions and answers from the file's content and formats them for a .sr file
+   */
+  public String extractQuestions() {
+    StringBuilder extractedQuestions = new StringBuilder();
+    boolean bracketed = false;
+    StringBuilder bracketContent = new StringBuilder();
+    for (int i = 0; i < content.length(); i++) {
+      char previous = setPrevious(content, i);
+      char current = content.charAt(i);
+      char next = setNext(content, i);
+
+      if (current == '[' && next == '[') {
+        bracketed = true;
+        bracketContent = new StringBuilder();
+      }
+      if (bracketed && current == ']' && next == ']') {
+        bracketed = false;
+        if (bracketContent.toString().contains(":::")) {
+          String question = formatQuestion(bracketContent);
+          extractedQuestions.append(question);
+        }
+      }
+      if (bracketed && !(current == '\n')
+          && !(current == '[' && (previous == '[' || next == '['))) {
+        bracketContent.append(current);
+      }
+
+    }
+    return extractedQuestions.toString();
+  }
+
+  /**
+   * Formats content as a question and answer
+   *
+   * @param content a valid question and answer denoted by :::
+   * @return the question and answer formatted for a .sr file
+   */
+  private String formatQuestion(StringBuilder content) {
+    StringBuilder formattedQuestion = new StringBuilder();
+    formattedQuestion.append("[Q]");
+    int index = 0;
+    while (!nextThree(content.toString(), index).equals(":::")) {
+      formattedQuestion.append(content.charAt(index));
+      index++;
+    }
+    formattedQuestion.append("[A]");
+    index += 3;
+    while (index < content.length()) {
+      formattedQuestion.append(content.charAt(index));
+      index++;
+    }
+    formattedQuestion.append("[D]HARD\n");
+    return formattedQuestion.toString();
+  }
+
+  /**
+   * Returns a String of the next three characters in the content String (including the current
+   * index)
+   *
+   * @param content the content to be parsed
+   * @param index the current index
+   * @return a String of the next three characters, including the current
+   */
+  private String nextThree(String content, int index) {
+    return String.valueOf(content.charAt(index))
+        + content.charAt(index + 1)
+        + content.charAt(index + 2);
+  }
+
+}
---
+++ src/test/java/cs3500/pa02/fileformatters/QuestionAndAnswerTest.java
@@ -0,0 +1,56 @@
+package cs3500.pa02.fileformatters;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class QuestionAndAnswerTest {
+  String arraysAndTest = """ 
+        # This is a test file
+                
+        ## Heading
+                
+        # Java Arrays
+        - [[An **array** is a collection of variables of the same type]], referred to
+          by a common name.
+        - In Java, arrays are objects, and must be created dynamically (at runtime).
+        - [[T/F An array can contain multiple types:::false]]
+                
+        ## Declaring an Array
+        - [[General Form: type[] arrayName;]]
+        - ex: int[] myData;
+        - [[T/F An array's size can be changed:::false]]
+                
+        - The above only creates a reference to an array object, but no array has
+          actually been created yet.
+                
+        ## Creating an Array (Instantiation)
+        - [[General form:  arrayName = new type[numberOfElements];]]
+        - ex: myData = new int[100];
+                
+        - Data types of the reference and array need to match.
+        - [[How do you initialize an integer array in Java?:::int[] = {..., ..., ...};]]
+        - [[numberOfElements must be a positive Integer.]]
+        - [[Gotcha: Array size is not modifiable once instantiated. ]]
+        - [[ Testing extra edge cases: ::: answer with ::]]
+        - [[ Testing on new line::: answer\s
+        is split up]]
+        - [[ T/F Testing with two colons ::]]
+                
+        ... more brilliance captured...\s
+        
+        """;
+  String formatted = """
+      [Q]T/F An array can contain multiple types[A]false[D]HARD
+      [Q]T/F An array's size can be changed[A]false[D]HARD
+      [Q]How do you initialize an integer array in Java?[A]int[] = {..., ..., ...};[D]HARD
+      [Q] Testing extra edge cases: [A] answer with ::[D]HARD
+      [Q] Testing on new line[A] answer is split up[D]HARD
+      """;
+
+  @Test
+  public void testExtractQuestions() {
+    assertEquals(formatted, new QuestionAndAnswer(arraysAndTest).extractQuestions());
+  }
+
+}
